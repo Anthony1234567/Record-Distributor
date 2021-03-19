9645524325c26a9623d9e3e123630bdf6b9e496b
Anthony1234567 <anthonyjmartinez94@gmail.com>
2021-03-18 21:19:37 -0700

    Add daily distribution counts

--- force-app/main/default/lwc/distributionListCard/distributionListCard.js
+++ force-app/main/default/lwc/distributionListCard/distributionListCard.js
@@ -5,6 +5,7 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getDistributionList from '@salesforce/apex/DistributionListCardController.getDistributionList';
 
 import DISTRIBUTION_SOURCE_ID_FIELD from '@salesforce/schema/Distribution_Rule__c.Distribution_Source_Id__c';
+import DAILY_CAP_FIELD from '@salesforce/schema/Distribution_Rule__c.Daily_Cap__c';
 
 export default class DistributionListCard extends LightningElement {
     @api recordId;
@@ -14,7 +15,8 @@ export default class DistributionListCard extends LightningElement {
     @wire(getRecord, {
         recordId: '$recordId',
         fields: [
-            DISTRIBUTION_SOURCE_ID_FIELD
+            DISTRIBUTION_SOURCE_ID_FIELD,
+            DAILY_CAP_FIELD
         ]
     }) distributionRule;
 
@@ -22,10 +24,13 @@ export default class DistributionListCard extends LightningElement {
         distributionSourceId: '$distributionSourceId'
     }) getDistributionList({ error, data }) {
         if (data) {
-            this.distributionList = data.map(user => {
+            this.distributionList = data.map(assigneeInfo => { 
                 return {
-                    ...user,
-                    relativeURL: `/${user.Id}`
+                    ...assigneeInfo,
+                    relativeURL: `/${assigneeInfo.assignee.Id}`,
+                    avatarSrc: !assigneeInfo.assignee.SmallPhotoUrl.endsWith('/profilephoto/005/T')
+                        ? assigneeInfo.assignee.SmallPhotoUrl
+                        : undefined
                 };
             });
         } else if (error) {
@@ -41,5 +46,9 @@ export default class DistributionListCard extends LightningElement {
 
     get distributionSourceId() {
         return getFieldValue(this.distributionRule.data, DISTRIBUTION_SOURCE_ID_FIELD);
+    }
+
+    get dailyCap() {
+        return getFieldValue(this.distributionRule.data, DAILY_CAP_FIELD);
     }
 }
